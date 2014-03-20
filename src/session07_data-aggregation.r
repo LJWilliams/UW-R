7de9a31ac5dbb9d9d8ca14ae0569e06d2cecd5a8
jennybc <jenny@stat.ubc.ca>
2014-03-19 23:34:53 -0700

    Clean up session07 code re data aggregation. Closes #1.

--- src/session07_data-aggregation.r
+++ src/session07_data-aggregation.r
@@ -1,9 +1,12 @@
 library(ggplot2)
+
+##install.packages("plyr", dependencies = TRUE)
 library(plyr)
 
-gDat <- read.delim("data//gapminderDataFiveYear.txt")
+gDat <- read.delim("../data/gapminderDataFiveYear.txt")
 str(gDat)
 
+## built-in apply() function operates on a MATRIX, not a data.frame
 tDat <- with(gDat,
              cbind(cambodia = lifeExp[country == "Cambodia"],
                    canada = lifeExp[country == "Canada"],
@@ -13,54 +16,67 @@ str(tDat)
 tDat
 
 apply(tDat, 1, mean)
-apply(tDat, 1, median)
+apply(tDat, 2, median)
 rowMeans(tDat)
 which.min(tDat[1, ])
 apply(tDat, 1, which.min)
+colnames(tDat)
 colnames(tDat)[apply(tDat, 1, which.min)]
-
 apply(tDat, 2, min)
-
 apply(tDat, 2, summary)
 apply(tDat, 2, quantile)
-apply(tDat, 2, quantile, probs = c(0.25, 0.75))
+apply(tDat, 2, quantile, probs = c(0.05, 0.95))
 
 ## challenge:
 ## use apply to compute the range of lifeExp for each country
 apply(tDat, 2, range)
-
 apply(tDat, 2, function(x) c(min = min(x), max = max(x)))
 
+## challenge:
+## compute the interquartile range of lifeExp by country
+## hint IQR()
+apply(tDat, 2, IQR)
+apply(tDat, 2, function(x) diff(quantile(x, probs = c(0.25, 0.75))))
+
+## leaving matrices and apply()
+
+## splitting up a vector based on a factor
+aggregate(lifeExp ~ continent, gDat, FUN = mean)
 str(aggregate(lifeExp ~ continent, gDat, FUN = mean))
 
-lifeExpByYearAndContinent <-
-  aggregate(lifeExp ~ year * continent, gDat, FUN = mean)
+aggregate(lifeExp ~ year * continent, gDat, FUN = mean)
+leByYearAndContinent <- aggregate(lifeExp ~ year * continent, gDat, FUN = mean)
+ggplot(leByYearAndContinent, aes(x = year, y = lifeExp, color = continent)) +
+  geom_line() + geom_point()
 
-ggplot(lifeExpByYearAndContinent,
-       aes(x = year, y = lifeExp, color = continent)) +
-  geom_line()
+aggregate(lifeExp ~ year * continent, gDat, quantile,
+          probs = c(0.05, 0.95))
 
-aggregate(country ~ continent, gDat,
-          function(x) length(unique(x)))
+# how many countries pre continent?
+aggregate(country ~ continent, gDat, function(x) length(unique(x)))
 
-nlevels(gDat$country)
+## challenge:
+## compute the min and max of gdpPercap for each country
+## ditto ... for 3 countries of your choice
+aggregate(gdpPercap ~ country, gDat, quantile, probs = c(0, 1))
+aggregate(gdpPercap ~ country, gDat, range)
+aggregate(gdpPercap ~ country, gDat, function(x) c(min(x), max(x)))
+aggregate(gdpPercap ~ country, gDat, range,
+          subset = country %in% c("Turkey", "Sudan", "Japan"))
 
-x <- aggregate(country ~ continent, gDat,
-               function(x) length(unique(x)))
-sum(x$country) == nlevels(gDat$country)
+## leaving single vectors and factors and aggregate() ...
 
+## ddply()
 library(plyr)
 
 ggplot(subset(gDat, country == "Zimbabwe"),
-       aes(x = year, y = lifeExp)) + geom_point(size = 7) +
-  geom_smooth(se = FALSE, method = "lm")
+       aes(x = year, y = lifeExp)) +
+  geom_point() + geom_smooth(se = FALSE, method = "lm")
 
 lm(lifeExp ~ year, gDat, subset = country == "Zimbabwe")
 
 (yearMin <- min(gDat$year))
-
-lm(lifeExp ~ I(year - yearMin), gDat,
-   subset = country == "Zimbabwe")
+lm(lifeExp ~ I(year - yearMin), gDat, subset = country == "Zimbabwe")
 
 jFit <- lm(lifeExp ~ I(year - yearMin), gDat,
            subset = country == "Zimbabwe")
@@ -72,11 +88,19 @@ jFun <- function(z) {
   names(jCoef) <- c("intercept", "slope")
   return(jCoef)
 }
+
 jFun(subset(gDat, country == "Zimbabwe"))
 jFun(subset(gDat, country == "Canada"))
 
-gCoef <- ddply(gDat, ~ country * continent, jFun)
+gCoef <- ddply(gDat, ~ country, jFun)
 str(gCoef)
 
-write.table(gCoef, "gCoef.txt", quote = FALSE, sep = "\t",
-            row.names = FALSE)
+gCoef <- ddply(gDat, ~ country + continent, jFun)
+str(gCoef)
+tail(gCoef)
+
+ggplot(gCoef, aes(x = intercept, y = slope)) + geom_point()
+ggplot(gCoef, aes(x = intercept, y = slope, color = continent)) +
+  geom_point()
+ggplot(gCoef, aes(x = continent, y = slope)) +
+  geom_jitter(position = position_jitter(width = 0.1))
