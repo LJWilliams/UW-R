e8b1edc277e45be271784a8c6c483799de021969
jennybc <jenny@stat.ubc.ca>
2014-03-19 23:07:50 -0700

    Revise more pipeline stuff to reflect new location

--- pipeline/02_slopeComparisonAsiaVsAmericas.R
+++ pipeline/02_slopeComparisonAsiaVsAmericas.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-str(gCoef <- readRDS("gCoef.rds"))
+str(gCoef <- readRDS("../results/gCoef.rds"))
 
 ## focus life expectancy trends in Asia vs. Americas
 hDat <-
@@ -11,7 +11,9 @@ str(hDat)
 ## inspect the slopes by continent
 p <- ggplot(hDat, aes(x = continent, y = slope)) + geom_point()
 print(p)
-ggsave("asia-vs-americas.pdf", plot = p)
+ggsave("../figs/asia-vs-americas.pdf", plot = p)
 
 ## test for a difference between Asia and Americas
 t.test(slope ~ continent, hDat)
+
+sessionInfo()
